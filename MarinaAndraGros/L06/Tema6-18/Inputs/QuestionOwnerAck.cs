dd96d2c171102c42c8477e9a82f4bf33e60df86e
unknown <andragros48@gmail.com>
2020-11-11 23:04:42 +0200

    Tema 6.2

---
+++ MarinaAndraGros/L06/Tema6-18/Inputs/QuestionOwnerAck.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tema6_18.Inputs
+{
+  
+        public class QuestionOwnerAck
+    {
+        public string Question { get; }
+
+        public QuestionOwnerAck(string question)
+        {
+            Question = question;
+        }
+    }
+  }
+
---
+++ MarinaAndraGros/L06/Tema6-18/Inputs/ReplyAuthorAck.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tema6_18.Inputs
+{
+    public class ReplyAuthorAck
+    {
+        public string ReplyAuthor { get; }
+
+        public ReplyAuthorAck(string replyauthor)
+        {
+            ReplyAuthor = replyauthor;
+        }
+    }
+}
